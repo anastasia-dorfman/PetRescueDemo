ec1b6d54cddf32dd0d4ea105e4c56d91b9318adc
anastasia-dorfman <anastasia.dorfman1@gmail.com>
2023-12-21 22:44:53 -0400

    PetInfo created

--- app/src/main/java/hoods/com/jetpetrescue/detail/Detail.kt
+++ app/src/main/java/hoods/com/jetpetrescue/detail/Detail.kt
@@ -3,55 +3,61 @@ package hoods.com.jetpetrescue.detail
 import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.ContentAlpha
 import androidx.compose.material.Icon
+import androidx.compose.material.LocalContentAlpha
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import hoods.com.jetpetrescue.components.PetBasicInfoItem
+import hoods.com.jetpetrescue.components.PetInfoItem
 import hoods.com.jetpetrescue.data.DummyPetDataSource
 import hoods.com.jetpetrescue.data.model.Pet
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun DetailScreen(index: Int, onNavigate: () -> Unit) {
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = { Text(text = "Detatil") },
-                navigationIcon = {
-                    Icon(
-                        imageVector = Icons.Default.ArrowBack,
-                        contentDescription = null,
-                        modifier = Modifier
-                            .size(24.dp, 24.dp)
-                            .clickable { onNavigate.invoke() },
-                        tint = MaterialTheme.colors.onSurface
-                    )
-                },
-                backgroundColor = MaterialTheme.colors.background,
-                contentColor = MaterialTheme.colors.onSurface
-            )
-        }
-    ) { padding ->
+    Scaffold(topBar = {
+        TopAppBar(title = { Text(text = "Detatil") },
+            navigationIcon = {
+                Icon(
+                    imageVector = Icons.Default.ArrowBack,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(24.dp, 24.dp)
+                        .clickable { onNavigate.invoke() },
+                    tint = MaterialTheme.colors.onSurface
+                )
+            },
+            backgroundColor = MaterialTheme.colors.background,
+            contentColor = MaterialTheme.colors.onSurface
+        )
+    }) { padding ->
         val pet = DummyPetDataSource.dogList[index]
         LazyColumn(contentPadding = padding) {
             item {
@@ -70,6 +76,14 @@ fun DetailScreen(index: Int, onNavigate: () -> Unit) {
             item {
                 MyStoryItem(pet = pet)
             }
+
+            item {
+                PetInfo(pet = pet)
+            }
+
+            item {
+                PetInfo(pet = pet)
+            }
         }
     }
 }
@@ -82,7 +96,9 @@ fun MyStoryItem(pet: Pet) {
         Spacer(modifier = Modifier.height(16.dp))
         Text(
             text = pet.description,
-            modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
             color = MaterialTheme.colors.onSurface,
             style = MaterialTheme.typography.body2,
             textAlign = TextAlign.Start
@@ -104,6 +120,66 @@ fun Title(title: String) {
     )
 }
 
+@Composable
+fun PetInfo(pet: Pet) {
+    Column {
+        Spacer(modifier = Modifier.height(24.dp))
+        Title(title = "Pet Info")
+        Spacer(modifier = Modifier.height(16.dp))
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceAround) {
+            InfoCard(
+                primaryText = pet.age,
+                secondaryText = "Age",
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(4.dp)
+            )
+            InfoCard(
+                primaryText = pet.color,
+                secondaryText = "Color",
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(4.dp)
+            )
+            InfoCard(
+                primaryText = pet.breed,
+                secondaryText = "Breed",
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(4.dp)
+            )
+        }
+    }
+}
+
+@Composable
+fun InfoCard(modifier: Modifier = Modifier, primaryText: String, secondaryText: String) {
+    Surface(shape = MaterialTheme.shapes.medium, modifier = modifier) {
+        Column(
+            modifier = Modifier.padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+//            Text(text = secondaryText, color = Color.Black.copy(alpha = .4f))
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.disabled) {
+                Text(text = secondaryText)
+            }
+            Text(
+                text = primaryText,
+                style = MaterialTheme.typography.body1,
+                fontWeight = FontWeight.Medium,
+                textAlign = TextAlign.Center,
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PrevInfoCard() {
+    InfoCard(primaryText = "Adult", secondaryText = "Age")
+}
 
 @Preview(showSystemUi = true)
 @Composable
